2c0e343a97df1fbd14c9c63a344f2dfe304ff462
Satish Wadkar <swadkar@rocketsoftware.com>
2024-09-19 13:29:25 +0530

    commit breaking code to test with AI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +13,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          Data is: {data.demoStr}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
